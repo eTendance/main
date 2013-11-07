7b5588201c7fff67d2ff1b9cbdefb1777e37ce16
Chris <cpellizzi@gmail.com>
2013-11-07 00:04:42 -0600

    -Improved checkin functionality. -Only 1 code can be generated for each class day in a particular class. -Added jquery date selector. -Added live checkin count for the current day (pulls the count every 10 seconds using ajax)

--- web-src/viewclass.php
+++ web-src/viewclass.php
@@ -4,8 +4,7 @@ require_once('global.php');
 check_auth('p');
 
 if (!isset($_GET['id'])) {
-    header('Location: professordashboard.php');
-    exit;
+    showdashboard();
 }
 
 
@@ -21,28 +20,51 @@ if (!empty($_REQUEST['action'])) {
 
     //this section will take care of any operations that occur on this page
 
-    if ($_GET['action'] == 'openenrollment') {
+    if ($_REQUEST['action'] == 'openenrollment') {
         mysql_query('UPDATE classes SET enrollmentopen="true" WHERE id="' . $classdata['id'] . '"');
-    } elseif ($_GET['action'] == "closeenrollment") {
+    } elseif ($_REQUEST['action'] == "closeenrollment") {
         mysql_query('UPDATE classes SET enrollmentopen="false" WHERE id="' . $classdata['id'] . '"');
-    } elseif ($_GET['action'] == 'newenrollmentcode') {
+    } elseif ($_REQUEST['action'] == 'newenrollmentcode') {
         mysql_query('UPDATE classes SET enrollmentcode="' . strtoupper(generateRandomString(10)) . '" WHERE id="' . $classdata['id'] . '"');
     }
 
-    if ($_GET['action'] == 'opencheckin') {
-        mysql_query('UPDATE checkincodes SET checkinopen="true" WHERE classid="' . $classdata['id'] . '" and code="'.mysql_real_escape_string($_GET['code']).'"');
-    } elseif ($_GET['action'] == "closecheckin") {
-        mysql_query('UPDATE checkincodes SET checkinopen="false" WHERE classid="' . $classdata['id'] . '" and code="'.mysql_real_escape_string($_GET['code']).'"');
+    if ($_REQUEST['action'] == 'opencheckin') {
+        mysql_query('UPDATE checkincodes SET checkinopen="true" WHERE classid="' . $classdata['id'] . '" and code="' . mysql_real_escape_string($_GET['code']) . '"');
+    } elseif ($_REQUEST['action'] == "closecheckin") {
+        mysql_query('UPDATE checkincodes SET checkinopen="false" WHERE classid="' . $classdata['id'] . '" and code="' . mysql_real_escape_string($_GET['code']) . '"');
     }
 
     if ($_REQUEST['action'] == 'generatecheckin') {
-        $checkincode = strtoupper(generateRandomString(10));
-        $query = 'INSERT INTO checkincodes (`code`,`classid`,`forclassday`,`checkinopen`) values("' . $checkincode . '","' . $classdata['id'] . '","' . mysql_real_escape_string($_POST['codeday']) . '","' . mysql_real_escape_string($_POST['open']) . '")';
-        mysql_query($query) or die(mysql_error());
+        $query = 'SELECT id FROM checkincodes where classid="' . $classdata['id'] . '" and forclassday="' . mysql_real_escape_string($_POST['codeday']) . '"';
+        $result = mysql_query($query) or die(mysql_error());
+        if (mysql_num_rows($result) > 0) {
+            $checkingenerationerror = 'A checkin code already exists for that class day.';
+            $NOREDIR = true;
+        }
+
+        if (!isset($checkingenerationerror)) {
+            $checkincode = strtoupper(generateRandomString(10));
+            $query = 'INSERT INTO checkincodes (`code`,`classid`,`forclassday`,`checkinopen`) values("' . $checkincode . '","' . $classdata['id'] . '","' . mysql_real_escape_string($_POST['codeday']) . '","' . mysql_real_escape_string($_POST['open']) . '")';
+            mysql_query($query) or die(mysql_error());
+        }
     }
 
 
-    header('Location: ' . $_SERVER['PHP_SELF'] . '?id=' . $_GET['id']);
+    /* return the number of users checked in, parameters are date, and id (of class) */
+    if ($_REQUEST['action'] == 'getdatecheckinsajax') {
+        $query = 'SELECT count(checkins.userid) FROM checkins '
+                . 'join checkincodes on checkincodes.id=checkins.checkincodeid '
+                . 'where checkins.classid = "' . $classdata['id'] . '" and '
+                . 'checkincodes.forclassday = "' . mysql_real_escape_string($_GET['date']) . '"';
+
+        $result = mysql_query($query) or die(mysql_error());
+        $row = mysql_fetch_row($result);
+        echo $row[0];
+        exit;
+    }
+
+    if (!isset($NOREDIR))
+        header('Location: ' . $_SERVER['PHP_SELF'] . '?id=' . $_GET['id']);
 }
 
 
@@ -61,6 +83,30 @@ while ($row = mysql_fetch_assoc($result)) {
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
+        <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+        <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+        <script>
+            function updatecheckincount() {
+                $.get("viewclass.php?id=<?php echo $_GET['id'] ?>&date=<?php echo date("Y-m-d"); ?>&action=getdatecheckinsajax", function(data) {
+                    $("#livecheckincount").html(data);
+                });
+            }
+            $(function() {
+                $("#codeday").datepicker({
+                    showOn: "button",
+                    buttonImage: "img/calendar.gif",
+                    buttonImageOnly: true,
+                    dateFormat: "yy-mm-dd"
+                });
+            });
+            $(document).ready(function() {
+                updatecheckincount();
+                setInterval(function() {
+                    updatecheckincount();
+                }, 10000);
+            });
+        </script>
     </head>
     <body>
         <h1><?php echo $classdata['name'] ?></h1><br />
@@ -85,32 +131,43 @@ while ($row = mysql_fetch_assoc($result)) {
 
 
         <h2>Checkin Codes</h2>
+
         Generate checkin codes for class days below.
+        <div class="errormsg"><?php if (isset($checkingenerationerror)) echo $checkingenerationerror; ?></div>
         <form action="" method="post">
             <input type="hidden" name="action" value="generatecheckin" />
-            Class Day: <input type="text" id="codeday" name="codeday" value="<?php echo date("Y-m-d"); ?>" />
+            Class Day: <input type="text" id="codeday" name="codeday" value="<?php echo date("Y-m-d"); ?>" /><br />
             Status: <input type="radio" name="open" value="true"/>Open <input type="radio" name="open" value="false" /> Closed
             <input type="submit" name="submit" value="Generate Code" />
         </form>
 
-        Last 5 codes generated:<ul>
-            <?php
-//select codes for this course
-            $result = mysql_query('SELECT * from checkincodes WHERE classid="' . mysql_real_escape_string($classdata['id']) . '" ORDER BY creationtime DESC LIMIT 5') or die(mysql_error());
-            if (mysql_num_rows($result) > 0) {
-                for ($i = 0; $row = mysql_fetch_assoc($result); $i++) {
-                    echo '<li>' . $row['code'] . ' for ' . $row['forclassday'] . ' ';
-                    if ($row['checkinopen'] == 'true') {
-                        echo '[<a href="' . $_SERVER['PHP_SELF'] . '?action=closecheckin&id=' . $classdata['id'] . '&code=' . $row['code'] . '">Open</a>]<br /><img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl='.$row['code'].'&choe=UTF-8" />';
-                    } else {
-                        echo '[<a href="' . $_SERVER['PHP_SELF'] . '?action=opencheckin&id=' . $classdata['id'] . '&code=' . $row['code'] . '">Closed</a>]';
+        <br />
+
+        <div>
+            Last 5 codes generated:<ul>
+                <?php
+//select and output the last 5 checkin codes created
+                $result = mysql_query('SELECT * from checkincodes WHERE classid="' . mysql_real_escape_string($classdata['id']) . '" ORDER BY creationtime DESC LIMIT 5') or die(mysql_error());
+                if (mysql_num_rows($result) > 0) {
+                    for ($i = 0; $row = mysql_fetch_assoc($result); $i++) {
+                        echo '<li>' . $row['code'] . ' for ' . $row['forclassday'] . ' ';
+                        if ($row['checkinopen'] == 'true') {
+                            echo '[<a href="' . $_SERVER['PHP_SELF'] . '?action=closecheckin&id=' . $classdata['id'] . '&code=' . $row['code'] . '">Open</a>]<br /><img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=' . $row['code'] . '&choe=UTF-8" />';
+                        } else {
+                            echo '[<a href="' . $_SERVER['PHP_SELF'] . '?action=opencheckin&id=' . $classdata['id'] . '&code=' . $row['code'] . '">Closed</a>]';
+                        }
+                        echo '</li>';
                     }
-                    echo '</li>';
+                } else {
+                    echo '<li>No checkin codes have been generated yet.</li>';
                 }
-            } else {
-                echo '<li>No checkin codes have been generated yet.</li>';
-            }
-            ?></ul>
-    </form>
-</body>
+                ?></ul>
+        </div>
+
+        <h2>Live Checkin Count</h2>
+        <div>
+            So far, <b><span id="livecheckincount"></span></b> class members have checked in today.
+        </div>
+
+    </body>
 </html>
