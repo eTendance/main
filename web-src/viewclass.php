99ee2911c30a11a8e5f9a8a4acc62924c82443cc
Chris <cpellizzi@gmail.com>
2013-11-16 02:18:57 -0600

    Someone removed the jquery UI stylesheet...added it back

--- web-src/viewclass.php
+++ web-src/viewclass.php
@@ -98,6 +98,7 @@ while ($row = mysql_fetch_assoc($result)) {
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
         <link rel="stylesheet" type="text/css" href="css/viewClass.css" />
+        <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
         <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
         <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
         <script>
